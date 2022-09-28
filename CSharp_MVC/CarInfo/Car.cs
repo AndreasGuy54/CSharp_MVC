d575aff5ce3e99775ec8b3900b6b040234d7cd32
Andreas Abaidoo <abaidooandy@gmail.com>
2022-09-28 15:33:54 +0000

    Add project files.

---
+++ CSharp_MVC/CarInfo/Car.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp_MVC.CarInfo
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public int Passengers { get; set; }
+        public string? Direction { get; set; }
+        public List<string> Wheels { get; set; } = new List<string>() 
+            { 
+                "LeftFront", "RightFront", "LeftRear", "RightRear"
+            };
+        public int Speed { get; set; }
+        public virtual void Accelerate() 
+        {
+            if (Speed <= 120)
+            {
+                Speed += 5;
+            }
+        }
+
+        public void Decelerate()
+        {
+            if (Speed >= 5)
+            {
+                Speed -= 5;
+            } 
+            else
+            {
+                Speed = 0;
+            }
+        }
+    }
+}
